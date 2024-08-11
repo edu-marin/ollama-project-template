340c699f8e078c55942231d8a3a11f8a810ef09d
Eduardo Marín Nicolalde <126416022+edu-marin@users.noreply.github.com>
2024-08-11 13:58:25 +0000

    fixed bug to pull llama3.1 model

--- setup.sh
+++ setup.sh
@@ -4,11 +4,13 @@
 curl -fsSL https://ollama.com/install.sh | sh
 
 # Start Ollama serve
-ollama serve &
+ollama serve
 
 # Pull llama3.1 model
 ollama pull llama3.1
 
 # Print custom message and list installed models
-echo "Ollama installed models are:"
 ollama list
+
+# Python test
+python test.py
